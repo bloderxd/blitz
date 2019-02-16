09be20cc115aad1958df60a57c631d25d9bd8c80
Daniel Martins Leme <aptgetinstalldanielleme@gmail.com>
2019-02-16 17:47:38 -0200

    Create example in Main Activity

--- app/src/main/java/bloder/com/blitz/MainActivity.kt
+++ app/src/main/java/bloder/com/blitz/MainActivity.kt
@@ -2,11 +2,19 @@ package bloder.com.blitz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import bloder.com.core.enableWhen
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.enableWhen {
+            edit1.isEmail()
+            edit2.isFilled()
+            edit3.isFollowingRegex("[^-?0-9]+")
+        }
     }
 }
