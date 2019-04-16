8ceaf6e75fc91f6221e7500eee2bbaf4bc72033f
Daniel Martins Leme <aptgetinstalldanielleme@gmail.com>
2019-04-15 22:19:26 -0300

    Build a complete example

--- app/src/main/java/bloder/com/blitz/CustomValidationExample.kt
+++ app/src/main/java/bloder/com/blitz/CustomValidationExample.kt
@@ -6,7 +6,7 @@ import bloder.com.blitzcore.validation.DefaultBlitzValidations
 
 class CustomValidationExample : DefaultBlitzValidations() {
 
-    fun CheckBox.wasChosen() : View = bindViewValidation(this) {
+    fun CheckBox.isAccepted() : View = bindViewValidation(this) {
         this.isChecked
     }
 }
--- app/src/main/java/bloder/com/blitz/MainActivity.kt
+++ app/src/main/java/bloder/com/blitz/MainActivity.kt
@@ -3,10 +3,10 @@ package bloder.com.blitz
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
+import bloder.com.blitzcore.enableWhen
 import bloder.com.blitzcore.enableWhenUsing
-import bloder.com.blitzcore.mask.then
 import bloder.com.blitzcore.mask.withMask
-import bloder.com.blitzcore.mask.withMaskSequence
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,17 +14,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        edit3 withMaskSequence("##/##" then "####-####" then "#####>###<#####")
-
-        button.enableWhenUsing(CustomValidationExample()) {
-            edit1.isEmail() onValidationSuccess {
-                arrow1.visibility = View.VISIBLE
-            } onValidationError {
-                arrow1.visibility = View.GONE
-            }
-            edit2.isFilled() withMask "###.##-####"
-            checkbox.wasChosen()
+        buildCustomFormValidation()
+        signup.setOnClickListener {
+            Toast.makeText(this, "Signed up", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun showSuccessCaseFor(successView: View, errorView: View) {
+        successView.visibility = View.VISIBLE
+        errorView.visibility = View.GONE
+    }
+
+    private fun showErrorCaseFor(successView: View, errorView: View) {
+        successView.visibility = View.GONE
+        errorView.visibility = View.VISIBLE
+    }
+
+    private fun buildCustomFormValidation() = signup.enableWhenUsing(CustomValidationExample()) {
+        email_field.isEmail() onValidationSuccess {
+            showSuccessCaseFor(email_success_icon, email_error_icon)
+        } onValidationError {
+            showErrorCaseFor(email_success_icon, email_error_icon)
+        }
+        document_field.isFilled() withMask "###.###.###-#"
+        terms.isAccepted()
+    }
+
+    private fun buildFormValidation() = signup.enableWhen {
+        email_field.isEmail()
+        document_field.isFilled()
     }
 }
