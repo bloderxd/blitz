d924dc7289c3d158ef7c12aec51ac4e57c67eb24
Heitor Ribeiro <heitors@vegait.com.br>
2020-02-03 13:47:56 -0300

    change retun to EditText in function withMask
    Signed-off-by: Heitor Ribeiro <heitors@vegait.com.br>

--- blitzcore/src/main/java/bloder/com/blitzcore/mask/MaskDsl.kt
+++ blitzcore/src/main/java/bloder/com/blitzcore/mask/MaskDsl.kt
@@ -18,15 +18,18 @@ private object MaskDsl {
     }
 }
 
-infix fun EditText.withMask(mask: String) = if (mask.isNotEmpty() && mask.isNotBlank()) {
-    MaskDsl.injectMask(this, BlitzMaskFormatter(this, mask))
-} else {}
+infix fun EditText.withMask(mask: String): EditText = if (mask.isEmpty() && mask.isBlank()) {
+    this
+} else {
+    (MaskDsl.injectMask(this, BlitzMaskFormatter(this, mask))).let { this }
+}
+
 
 infix fun EditText.withMaskSequence(masks: MutableList<String>) = MaskDsl.injectMask(this, BlitzMaskFormatter(this, masks.flatMap {
     listOf(
             BlitzMaskFormatter.Mask(it, it.filter { char -> char == '#' }.length))
 }))
 
-infix fun String.then(mask: String) : MutableList<String> = mutableListOf(this, mask)
+infix fun String.then(mask: String): MutableList<String> = mutableListOf(this, mask)
 
-infix fun MutableList<String>.then(mask: String) : MutableList<String> = this.also { it.add(mask) }
+infix fun MutableList<String>.then(mask: String): MutableList<String> = this.also { it.add(mask) }
